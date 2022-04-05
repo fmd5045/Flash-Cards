d844a2e43805f9e3f0141154c76b137e1212756f
fmd5045 <freddy.duluc@gmail.com>
2022-04-05 10:35:39 -0600

    Created a form to add new cards Added the create route to the cards controller. This will allow the user to add more cads to the database.

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,4 +2,17 @@ class CardsController < ApplicationController
     def index
         @cards= Card.all
     end
+
+    def new
+        @card = Card.new
+    end
+
+    def create
+        @card = Card.new(card_params)
+        @card.save
+    end
+    private
+    def card_params
+        params.require(:card).permit(:term,:definition)
+    end
 end
