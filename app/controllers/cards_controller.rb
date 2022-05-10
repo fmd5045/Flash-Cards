8e499214ff91bb237ff68bf9f226bd38fc96a52f
fmd5045 <freddy.duluc@gmail.com>
2022-05-10 09:24:42 -0600

    Adding validation on the term Added validation to verify the uniqueness of the term being added. Also allowed the error message to populate when the term is not saved properly.

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -3,14 +3,51 @@ class CardsController < ApplicationController
         @cards= Card.all
     end
 
+    def show
+        @card = Card.find(params[:id])
+    end
+
     def new
         @card = Card.new
     end
 
+    def edit
+        @card = Card.find(params[:id])
+    end
+
+    def update
+        @card = Card.find(params[:id])
+        if @card.update(card_params)
+            flash[:success] = "You've successfullly updated a card!"
+            redirect_to cards_path
+        else
+            flash[:error] = @card.errors.full_messages.first
+            redirect_to edit_card_path(@card)
+        end
+    end
+
     def create
         @card = Card.new(card_params)
-        @card.save
+        if @card.save
+            flash[:success] = "You've successfullly created a card!"
+            redirect_to cards_path
+        else
+            flash[:error] = @card.errors.full_messages.first
+            redirect_to new_card_path
+        end
     end
+
+    def destroy
+        @card = Card.find(params[:id])
+        @card.destroy
+        flash[:success] = "You've successfullly deleted a card!"
+        redirect_to cards_path  
+        # else
+        #     flash.now[:error] = @card.errors.full_messages.first
+        #     render :index
+        # end
+    end
+
     private
     def card_params
         params.require(:card).permit(:term,:definition)
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,4 @@
 class Card < ApplicationRecord
+    validates :term, :definition, presence: true
+    validates :term, uniqueness: true
 end
